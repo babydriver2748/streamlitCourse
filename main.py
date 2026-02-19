48de6e9fd3ae096ef21f1759ca756052353f1937
babydriver2748 <avishekkumar5665@gamil.com>
2026-02-19 12:41:29 +0530

    forth

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ st.chat_input("Enter you message")
 
 st.audio_input("Record Message for Me")
 chai = st.selectbox("Select you favroit Chai:",["Desi Chai", "Masala Chai","Lemon chai", "black tea"])
+
+st.write(f"You Choice is great {chai}")
+
+st.success("your Order is placed")
